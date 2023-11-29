7ad36f3c49ffa598a592f764e7233c08e778bbb5
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-11-29 12:53:09 +0300

    генерировать код

--- src/phone/phone.controller.ts
+++ src/phone/phone.controller.ts
@@ -6,19 +6,18 @@ export class PhoneController {
   constructor(private readonly phoneService: PhoneService) {}
 
   @Post('getcode')
-    async getCode(@Body('phone') phone: string, @Body('code') code: string): Promise<void> {
-    const context = {
-      phone: phone,
-      code: code
-    };
-    this.phoneService.setContext(context);
-    console.log(this.phoneService.getContext())
-    await this.phoneService.getCode(context.phone, context.code);
+  async getCode(@Body('phone') phone: string): Promise<void> {
+    const context = { phone: phone };
+    // this.phoneService.setContext(context);
+    // console.log(this.phoneService.getContext());
+    await this.phoneService.getCode(context.phone);
   }
 
   @Post('check')
   async check(@Body('code') code: string): Promise<boolean> {
     const generatedCode = await this.phoneService.getGeneratedCode();
+    console.log(`${generatedCode} ${code}`);
+
     return code === generatedCode;
   }
 }
--- src/phone/phone.service.ts
+++ src/phone/phone.service.ts
@@ -5,21 +5,27 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PhoneService {
-    constructor(private httpService: HttpService, private readonly configService: ConfigService) {}
+    constructor(
+        private readonly httpService: HttpService,
+        private readonly configService: ConfigService
+    ) {}
 
     private generatedCode: string;
     private context: any;
 
-    async getCode(phone: string, code: string): Promise<boolean> {
-        this.generatedCode = code;
-
+    async getCode(phone: string): Promise<boolean> {
+        this.generatedCode = Math.floor(
+            Math.random() * (999999 - 100000 + 1) + 100000
+        ).toString();
+        console.log(`сгенерированный код ${this.generatedCode}`);
+        this.setContext(this.generatedCode);
         const url = `${this.configService.get('PHONE_API_URL')}`;
         const data = {
             messages: [
                 {
-                   from: this.configService.get('SENDERS_NAME'),
-                   to: phone,
-                   text: code,
+                    from: this.configService.get('SENDERS_NAME'),
+                    to: phone,
+                    text: this.generatedCode,
                     validity: 0,
                     scheduledTime: '2022-09-09T08:52:27.319Z',
                     priority: 'HIGH',
@@ -33,13 +39,14 @@ export class PhoneService {
         const response$ = this.httpService.post(url, data, { headers });
 
         return response$.pipe(
-            map((response) => response.data.status === 'ok'),
+            map((response) => response.data.status === 'ok')
         ).toPromise();
     }
 
     async getGeneratedCode(): Promise<string> {
         return this.generatedCode;
     }
+
     setContext(context: any) {
         this.context = context;
     }
